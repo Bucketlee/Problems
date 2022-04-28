0efa9deea23ba2c6700db57af0d16ef5f6b5864a
Chanyoung <chanyoung.cedu@gmail.com>
2022-04-29 06:39:52 +0900

    feat: make template (node.js in baekjoon)

---
+++ Baekjoon/template.js
@@ -0,0 +1,6 @@
+const fs = require('fs');
+const path = require('path');
+const filePath = process.platform === 'linux' ? '/dev/stdin' : path.join(__dirname, '/example.txt');
+const input = fs.readFileSync(filePath).toString().split('\n');
+
+console.log(input);
