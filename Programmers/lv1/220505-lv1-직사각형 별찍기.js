b7fb2bed96fa1cc2be8f1234134abac249dcfaa8
Chanyoung <chanyoung.cedu@gmail.com>
2022-05-05 23:40:07 +0900

    feat(problem): solve makeStarts

---
+++ Programmers/lv1/220505-lv1-직사각형 별찍기.js
@@ -0,0 +1,27 @@
+// https://programmers.co.kr/learn/courses/30/lessons/12969
+
+process.stdin.setEncoding('utf8');
+process.stdin.on('data', data => {
+  const n = data.split(" ");
+  const a = Number(n[0]), b = Number(n[1]);
+  for (let i = 0; i < b; i += 1) {
+    let result = '';
+    for (let j = 0; j < a; j += 1) {
+      result = result + '*';
+    }
+    console.log(result);
+  }
+});
+
+
+// 다른이 풀이
+
+process.stdin.setEncoding('utf8');
+process.stdin.on('data', data => {
+  const n = data.split(" ");
+  const a = Number(n[0]), b = Number(n[1]);
+  const row = '*'.repeat(a)
+  for(let i =0; i < b; i++){
+    console.log(row)
+  }
+});
