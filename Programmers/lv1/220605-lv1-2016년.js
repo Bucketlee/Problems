d4a4e0b36fdfba484e361ff7d03ba772e57e3a35
Chanyoung <chanyoung.cedu@gmail.com>
2022-06-05 23:44:45 +0900

    feat(programmers): solve 2016

---
+++ Programmers/lv1/220605-lv1-2016년.js
@@ -0,0 +1,16 @@
+// https://programmers.co.kr/learn/courses/30/lessons/12901
+
+function solution(a, b) {
+  const days = {
+    0: 'SUN',
+    1: 'MON',
+    2: 'TUE',
+    3: 'WED',
+    4: 'THU',
+    5: 'FRI',
+    6: 'SAT',
+  }
+  const date = new Date(2016, a-1, b);
+  const day = date.getDay();
+  return days[day];
+}
