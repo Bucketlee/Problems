5fcb791ad397f9809cd519e55b19a9c4623ebbce
Chanyoung <chanyoung.cedu@gmail.com>
2022-06-12 22:24:22 +0900

    feat(programmers): solve 나누어 떨어지는 숫자 배열

---
+++ Programmers/lv1/220612-lv1-나누어 떨어지는 숫자 배열.js
@@ -0,0 +1,6 @@
+// https://programmers.co.kr/learn/courses/30/lessons/12910v
+
+function solution(arr, divisor) {
+  const result = arr.sort((a, b) => a-b).filter(num => num%divisor === 0);
+  return result.length === 0 ? [-1] : result;
+}
