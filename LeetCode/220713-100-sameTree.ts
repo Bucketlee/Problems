2a0d45c3f02a901311927f0aeeaca352e5ab5d14
Chanyoung <chanyoung.cedu@gmail.com>
2022-07-13 23:13:08 +0900

    feat(leetcode): solve same tree

---
+++ LeetCode/220713-100-sameTree.ts
@@ -0,0 +1,21 @@
+// https://leetcode.com/problems/same-tree/
+
+class TreeNode {
+  val: number
+  left: TreeNode | null
+  right: TreeNode | null
+  constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+    this.val = (val===undefined ? 0 : val)
+    this.left = (left===undefined ? null : left)
+    this.right = (right===undefined ? null : right)
+  }
+}
+
+function isSameTree(p: TreeNode | null, q: TreeNode | null): boolean {
+  if (!p && !q) return true;
+  if (!p || !q || p.val !== q.val) return false;
+  return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+};
+
+// Runtime: 126 ms, faster than 8.47% of TypeScript online submissions for Same Tree.
+// Memory Usage: 44.6 MB, less than 44.35% of TypeScript online submissions for Same Tree.
