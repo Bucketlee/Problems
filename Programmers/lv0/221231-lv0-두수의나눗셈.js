658c6c6347f97d0db94256327994af3648c516d0
Chanyoung <chanyoung.cedu@gmail.com>
2022-12-31 00:02:21 +0900

    feat(programmers): solve 두수의 나눗셈

---
+++ Programmers/lv0/221231-lv0-두수의나눗셈.js
@@ -0,0 +1,10 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/120806
+
+function solution(num1, num2) {
+  return parseInt(num1 / num2 * 1000);
+}
+
+
+// 다른이풀이
+
+const solution = (num1, num2) => Math.floor(num1 / num2 * 1000)
