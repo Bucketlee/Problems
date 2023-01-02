128a5d4ae7e1545e9b11cdb8d29d3ee1a1946808
Chanyoung <chanyoung.cedu@gmail.com>
2023-01-02 18:27:52 +0900

    feat(programmers): solve 배열 두배 만들기

---
+++ Programmers/lv0/230102-lv0-배열_두배_만들기.js
@@ -0,0 +1,5 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/120809https://school.programmers.co.kr/learn/courses/30/lessons/120809
+
+function solution(numbers) {
+  return numbers.map(num => num*2);
+}
